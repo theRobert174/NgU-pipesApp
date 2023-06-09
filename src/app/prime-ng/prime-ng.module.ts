87ff478a11dae9c2eb8f6f0883b110bb9a756aae
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-08 23:41:38 -0600

    primeng menubar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
 
 
 
 @NgModule({
   exports: [
+    ButtonModule,
+    MenubarModule,
     MenuModule,
-    ButtonModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -14,12 +14,32 @@ export class MenuComponent implements OnInit{
     ngOnInit() {
         this.menuItems = [
             {
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
+              label: 'Pipes de Angular',
+              icon: 'pi pi-desktop',
+              items: [
+                {
+                  label: 'Textos y Fechas',
+                  icon: 'pi pi-align-left'
+                },
+                {
+                  label: 'Numeros',
+                  icon: 'pi pi-dollar'
+                },
+                {
+                  label: 'No comunes',
+                  icon: 'pi pi-globe'
+                },
+              ]
             },
             {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-trash'
+              label: 'Pipes Personalizados',
+              icon: 'pi pi-cog',
+              items: [
+                {
+                  label: 'Otro elemento',
+                  icon: 'pi pi-cog'
+                }
+              ]
             }
         ];
     }
